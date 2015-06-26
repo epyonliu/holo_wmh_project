752aca643752404798ffe02422bb9e88df4b5151
Jianxiong Liu <mydendrobium@hotmail.com>
2015-06-26 17:23:47 +0100

    first submit
    Initial submit of the unity project and the FD doc

---
+++ holo_wmh_unity/Assets/Models/CModelProperty.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CModelProperty : MonoBehaviour {
+
+	// Model stats
+	public int Spd, Str, Mat, Rat, Arm, Def, Cmd;
+
+	// Model status
+	public bool Fire_Cont;       // model in fire continuous effect
+	public bool Corrosion_cont;  // model in corrosion continuous effect
+	public bool Stationary;      // model is stationary
+	public bool Knocked_down;    // model is knocked down
+	public bool Fleeing;         // model is fleeing
+	public bool Disabled;        // model is disabled
+	public bool Boxed;           // model is boxed
+	public bool Destroyed;       // model is destroyed
+	public bool RFP;             // model is removed from play
+
+	// Model abilities
+	public bool Fearless;
+	public bool Terror;
+	public bool Abomination;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ holo_wmh_unity/Assets/Scripts/CCameraMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CCameraMovement : MonoBehaviour {
+
+	public float RotateSpeed;
+	Camera MainCam;
+
+	// Use this for initialization
+	void Start () {
+		MainCam = GetComponent<Camera> ();
+		MainCam.transform.RotateAround(MainCam.transform.position,Vector3.right,45);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xAxisValue = Input.GetAxis("Horizontal");
+		float zAxisValue = Input.GetAxis("Vertical");
+		MainCam.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+		if (Input.GetMouseButton(1)) {
+			float MouseHorizontal = Input.GetAxis("Mouse X");
+			float MouseVertical = -1.0f * Input.GetAxis("Mouse Y");
+			Vector3 CamAngle = MainCam.transform.eulerAngles;
+
+
+
+			//MainCam.transform.Rotate (0,MouseHorizontal*Time.deltaTime*RotateSpeed,0,Space.World);
+			//MainCam.transform.Rotate (MouseVertical*Time.deltaTime*RotateSpeed,0,0,Space.World);
+
+			MainCam.transform.RotateAround(MainCam.transform.position, Vector3.down, MouseHorizontal*Time.deltaTime*RotateSpeed);
+
+			//MainCam.transform.RotateAround(MainCam.transform.position,Vector3.right,MouseVertical*Time.deltaTime*RotateSpeed);
+	
+
+	
+
+
+			//MainCam.transform.RotateAround(MainCam.transform.position, Vector3.down, 20);
+
+			//MainCam.transform.eulerAngles = new Vector3(CamAngle.x, MainCam.transform.eulerAngles.y, MainCam.transform.eulerAngles.z);
+			//MainCam.transform.eulerAngles.Set (45f,MainCam.transform.eulerAngles.y,MainCam.transform.eulerAngles.z);
+			//print (string.Concat("before: ", CamAngle.x, "   after: ", MainCam.transform.eulerAngles.x));
+
+		}
+	}
+}
